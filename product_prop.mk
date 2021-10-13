442094150bb719f7705b4a99187b3d4f9d650a02
Kichu <84064718+kichuk178@users.noreply.github.com>
2021-10-13 15:27:24 +0530

    bringup aosp

--- product_prop.mk
+++ product_prop.mk
@@ -1,8 +1,8 @@
-#Blur
+# Blurs
 PRODUCT_PRODUCT_PROPERTIES += \
-ro.sf.blurs_are_expensive=1 \
-ro.surface_flinger.supports_background_blur=1 \
-debug.sf.disable_backpressure=1
+    ro.surface_flinger.supports_background_blur=1 \
+    persist.sys.sf.disable_blurs=1 \
+    ro.sf.blurs_are_expensive=1
 
 # Dex2oat
 PRODUCT_PROPERTY_OVERRIDES += \
@@ -15,10 +15,13 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Graphics
 PRODUCT_PRODUCT_PROPERTIES += \
     debug.hwui.renderer=skiavk \
+    ro.surface_flinger.max_frame_buffer_acquired_buffers=3 \
     debug.sf.latch_unsignaled=1 \
-    ro.surface_flinger.use_color_management=true \
+    debug.sf.disable_backpressure=1 \
+    debug.sf.disable_hwc_overlays=1 \
     ro.hardware.egl=adreno \
-    ro.hardware.vulkan=adreno
+    ro.hardware.vulkan=adreno \
+    ro.surface_flinger.use_color_management=true
 
 # Media
 PRODUCT_PROPERTY_OVERRIDES += \
@@ -32,4 +35,11 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.ril.disable.power.collapse=0 \
     power.saving.mode=1 \
     pm.sleep_mode=1
- 
+
+# PixelPlusUI Official
+PRODUCT_PRODUCT_PROPERTIES += \
+ro.ppui.device_name=Juice \
+ro.ppui.version=3.10 \
+ro.ppui.version_code=Azrael \
+ro.ppui.is_official=true \
+ro.ppui.maintainer_name=SreeGovindSA
